5a6bef5784a4be6768f272b6945a49e62e2e658b
Escaity <112839844+d4ken@users.noreply.github.com>
2023-03-15 23:25:28 +0900

    feat:犬の画像を取得するAPIを使って画像を取得するコンポーネントを作成

---
+++ src/components/DogApi.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img v-if="data" :src="data"/>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const data = ref(null)
+
+async function fetchData() {
+  const response = await axios.get('https://dog.ceo/api/breeds/image/random')
+  let temp = response.data
+  data.value = temp.message
+}
+
+onMounted(fetchData)
+
+</script>
+
+<style scoped>
+
+img {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: block;
+  width: 100%;
+  aspect-ratio: 16 / 9;
+  object-fit: cover;
+}
+
+</style>
