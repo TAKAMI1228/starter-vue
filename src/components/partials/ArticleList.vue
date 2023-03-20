0df252f677f3da48f59a1e20aaf4c594f21c95ad
Escaity <112839844+d4ken@users.noreply.github.com>
2023-03-20 22:57:14 +0900

    fix:リンクを設定

--- src/components/partials/ArticleList.vue
+++ src/components/partials/ArticleList.vue
@@ -1,15 +1,23 @@
 <template>
   <div style="padding: 0 20px 20px 0;">
-    <v-card>
-      <v-list :items="items"></v-list>
-    </v-card>
+    <v-list>
+      <v-list-item
+          v-for="item in items"
+          :key="item.title"
+      >
+        <router-link to="/shinchoku">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </router-link>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script setup>
 const items = [
-  { title: 'content1' },
-  { title: 'content2' },
-  { title: 'content3' },
+  {title: 'content1'},
+  {title: 'content2'},
+  {title: 'content3'},
 ]
+
 </script>
